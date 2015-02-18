b5b7c4b73cf4954e88ec9769d3730ab499217f84
Mike <mike.toggweiler@tegonal.com>
2015-02-18 15:42:59 +0100

    added support for server messaged in booking history

--- app/assets/javascripts/directives/lasBookingHistory.js
+++ app/assets/javascripts/directives/lasBookingHistory.js
@@ -3,7 +3,7 @@ define(['angular'], function(angular) {
   'use strict';
 
   var mod = angular.module('directives.lasBookingHistory', []);
-  mod.directive('lasBookingHistory', ['bookingHistoryService', 'moment', function(bookingHistoryService, moment) {
+  mod.directive('lasBookingHistory', ['bookingHistoryService', 'msgBus', 'moment', function(bookingHistoryService, msgBus, moment) {
     return {
       restrict: 'E',
       transclude: true,
@@ -40,6 +40,33 @@ define(['angular'], function(angular) {
         scope.diff = function(booking) {
           return moment.duration(booking.end).subtract(booking.start).asHours();
         };
+        
+        msgBus.onMsg('UserTimeBookingHistoryEntryAdded', scope, function(
+            event, msg) {
+          console.log('msg received' + msg.type);
+          if (scope.userId === msg.booking.userId && scope.date.startOf('day').unix() === moment(msg.booking.start).startOf('day').unix()) {
+            scope.bookings.push(msg.booking);
+            scope.$apply();
+          }          
+        });
+        
+        msgBus.onMsg('UserTimeBookingHistoryEntryRemoved', scope, function(
+            event, msg) {
+          console.log('msg received' + msg.type);
+          for(i=0;i<scope.bookings.length;i++){
+            if(scope.bookings[i].id === msg.bookingId){
+              scope.bookings.splice(i, 1);
+            }
+          }               
+        });
+        
+        msgBus.onMsg('UserTimeBookingHistoryEntryCleaned', scope, function(
+            event, msg) {
+          console.log('msg received' + msg.type);
+          if (scope.userId === msg.userId) {
+            scope.bookings.clear();            
+          }            
+        });
       }
     };
   }]);
