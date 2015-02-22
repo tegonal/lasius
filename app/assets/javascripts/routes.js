035e02e314bbbad2ea5a62c8671f1dcc1d7daad0
toggm <mike.toggweiler@tegonal.com>
2015-02-22 22:52:56 +0100

    adjusted main layout

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -10,7 +10,7 @@ define(['angular',
 
   mod.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/', {templateUrl: '/assets/dashboard.html', controller:dashboardController.DashboardCtrl})
+      .when('/', {templateUrl: '/assets/home.html', controller:dashboardController.DashboardCtrl})
       .otherwise({redirectTo: '/assets/notFound.html'});
   }]);
   
