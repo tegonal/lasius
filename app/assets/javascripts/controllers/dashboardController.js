3cbc601cde02c17a50bb290577596be2ab5ac10e
Mike <mike.toggweiler@tegonal.com>
2015-02-24 07:42:25 +0100

    adjusted animation of content

--- app/assets/javascripts/controllers/dashboardController.js
+++ app/assets/javascripts/controllers/dashboardController.js
@@ -4,13 +4,21 @@
 define(['angular'], function(angular) {
   'use strict';
 
-  var DashboardCtrl = function($q, $log, $scope, $rootScope, moment) {
+  var DashboardCtrl = function($q, $log, $scope, $rootScope, $animate, $document, moment) {
     
     $scope.date = moment();
     $scope.currentBookingOn = true;
     
     $scope.showCurrentBooking = function(on) {
       $scope.currentBookingOn = on;
+      var body = $document[0].body;
+      var content = $document[0].getElementById('content_body');
+      if (on) {
+        $animate.setClass(content, 'main_content', 'main_content_closed');
+      }
+      else {        
+        $animate.setClass(content, 'main_content_closed', 'main_content');
+      }
     };
     
     $scope.dayMinus = function() {
@@ -36,7 +44,7 @@ define(['angular'], function(angular) {
     };
   };
   
-  DashboardCtrl.$inject = ['$q', '$log', '$scope', '$rootScope', 'moment'];  
+  DashboardCtrl.$inject = ['$q', '$log', '$scope', '$rootScope', '$animate', '$document', 'moment'];  
 
   return {
     DashboardCtrl: DashboardCtrl
