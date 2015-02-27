309761d1fab71ee4cb813cfade79e44894d82017
toggm <mike.toggweiler@tegonal.com>
2015-02-27 09:54:28 +0100

    added example donut pie chart as current workview

--- app/assets/javascripts/directives/lasWorkviewPieChart.js
+++ app/assets/javascripts/directives/lasWorkviewPieChart.js
@@ -3,16 +3,18 @@ define(['angular'], function(angular) {
   'use strict';
 
   var mod = angular.module('directives.lasWorkviewPieChart', []);
-  mod.directive('lasWorkviewPieChart', ['bookingStatisticsService', 'msgBus', 'moment', function(bookingStatisticsService, msgBus, moment) {
+  mod.directive('lasWorkviewPieChart', ['$window', 'bookingStatisticsService', 'msgBus', 'moment', function($window, bookingStatisticsService, msgBus, moment) {
     return {
       restrict: 'E',
       templateUrl: '/assets/directives/las-workview-pie-chart-tmpl.html',
       scope:  {
-        userId:'=',
-        source:'=',
-        range:'='
+        userId:'='
       },
       link: function(scope, iElement, iAttrs) {
+        
+        var activeTimeout;
+        var currentValue;
+        var unwatchChanges;
         
         scope.xFunction = function(){
           return function(d) {
@@ -25,30 +27,95 @@ define(['angular'], function(angular) {
           };
         };
         
-        var pattern = 'DDMMYYYYHHmmss';        
+        var pattern = 'DDMMYYYYHHmmss';
         
-        var load = function(range) {
-          if (range === undefined || range.from === undefined) {
+        scope.margin = function(index, offset) {
+          return {
+            left:-index*offset,
+            top:-index*offset,
+            bottom:0,
+            right:0
+          };                   
+        };
+        
+        var limit = moment.duration(8, 'hours').asMilliseconds();
+        scope.charts = [];
+        
+        var generateCharts = function() {          
+          var numberOfCharts = Math.ceil(scope.result.total / limit);
+          var fullChartData = [
+                               {
+                                 label: "worked",
+                                 value: 100
+                               },
+                               {
+                                 label: "open",
+                                 value: 0
+                               }
+                               ];
+          
+          //push 100% workload charts
+          for (var i = scope.charts.length; i < numberOfCharts-1; i++) { 
+            scope.charts.push(fullChartData);
+          }
+          //set chart-2 to 100%
+          if (scope.charts[numberOfCharts-2][0].value != 100) {
+            scope.charts[numberOfCharts-2] = fullChartData;              
+          }
+          
+          //push rest
+          var rest = scope.result.total - (numberOfCharts-1)*limit;
+          var open = limit - rest;
+          
+          if (scope.charts.length < numberOfCharts) {
+            //insert new
+            scope.charts.push([
+                               {
+                                 label: "worked",
+                                 value: rest
+                               },
+                               {
+                                 label: "open",
+                                 value: open
+                               }
+                               ]);
+          }
+          else {
+            //update
+            scope.charts[numberOfCharts-1] = [
+                                              {
+                                                label: "worked",
+                                                value: rest
+                                              },
+                                              {
+                                                label: "open",
+                                                value: open
+                                              }
+                                              ];
+          }
+        };
+        
+        var load = function(date) {
+          if (date === undefined) {
             return;
           }
-          var from = range.from.format(pattern);
-          var to = range.to.format(pattern);
+          var from = date.format(pattern);
+          var to = date.format(pattern);
           
-          bookingStatisticsService.getAggregatedStatistics(scope.source, scope.userId, from, to).then(function(statistics) {
+          /*bookingStatisticsService.getAggregatedStatistics(scope.source, scope.userId, from, to).then(function(statistics) {
             scope.statistics = statistics;
-          });
-        };
-        
-        scope.$watch('range',
-            function(value){
-              load(value);                              
-            }, true);
-        
-        scope.callbackFunction = function(){
-          return function(chart){
-              console.log('inner callback function', chart);
-          };
-        };
+          });*/
+          
+          scope.result = {
+              total: moment.duration(10, 'hours').asMilliseconds(),
+              booking: {
+                start: moment.duration(1, 'hours').asMilliseconds()
+              }
+          };                            
+          generateCharts();          
+       };
+       
+       load(moment());
         
         function cancelTimer() {
           if (activeTimeout) {
@@ -58,16 +125,19 @@ define(['angular'], function(angular) {
         }
         
         function updateTime(momentInstance, apply) {
-          scope.duration.moment = moment().subtract(momentInstance);
+
+          // update every minute
+          var millis = 60000;
+          scope.result.total += moment.duration(millis, 'milliseconds').asMilliseconds();          
+          generateCharts();   
           
           if (apply) {
             scope.$apply();
           }
-
-          // update every minute
+          
           activeTimeout = $window.setTimeout(function() {
             updateTime(momentInstance, true);
-          }, 60000);
+          }, millis);
         }
         
         msgBus.onMsg('CurrentUserTimeBooking', scope, function(
@@ -93,7 +163,7 @@ define(['angular'], function(angular) {
           }
         }
         
-        unwatchChanges = scope.$watch('booking.start',
+        unwatchChanges = scope.$watch('result.booking.start',
             function(value) {
               if ((typeof value === 'undefined') || (value === null) || (value === '')) {
                 cancelTimer();
