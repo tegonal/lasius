76ec921d072ab68de819aebea50793edba3361cc
toggm <mike.toggweiler@tegonal.com>
2015-03-02 21:05:35 +0100

    added total row to booking history

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -23,6 +23,28 @@ define(['angular'], function(angular) {
     };
   }]);
   
+  mod.filter('sumFilter', function() {
+    return function(items, property) {
+        var total = 0;
+        if (items === undefined) {
+          return undefined;
+        }
+        for (var i=0; i<items.length; i++) {
+          var value = items[i];
+          if (angular.isObject(value)) {
+            if (value.hasOwnProperty(property)) {
+              total = total + value[property];
+            }
+            else if (angular.isFunction(property)) {
+              var val = property(value);              
+              total = total + val;
+            }
+          }
+         }
+        return total;
+    };
+  });
+  
   /**
    * AngularJS default filter with the following expression:
    * "person in people | filter: {name: $select.search, age: $select.search}"
