ea7b743814e376c0658dc5df6964ba1cb851b6da
toggm <mike.toggweiler@tegonal.com>
2015-03-24 21:59:30 +0100

    LAS-12: fixed actor based testcase

--- test/domain/LoginStateAggregateSpec.scala
+++ test/domain/LoginStateAggregateSpec.scala
@@ -16,42 +16,50 @@ import domain.AggregateRoot._
 import org.junit.runner.RunWith
 import org.specs2.time.NoTimeConversions
 import akka.ActorSpecs
+import akka.testkit.TestProbe
+import akka.actor.Props
+import akka.actor.Actor
+import akka.event.LoggingReceive
+import akka.testkit.TestActorRef
+import org.specs2.matcher.Scope
 
-@RunWith(classOf[JUnitRunner])
-class LoginStateAggregateSpec extends ActorSpecs {
+class LoginStateAggregateSpec extends Specification {
 
-  val actorRef = system.actorOf(LoginStateAggregate.props)
-  //system.eventStream.subscribe(testActor, classOf[LoggedInState])
+  class Actors extends TestKit(ActorSystem("test")) with Scope
 
   "LoginStateggregate updateState" should {
-    "user login" in {
+    "user login" in new Actors {
+      val probe = TestProbe()
+      val actorRef = system.actorOf(LoginStateAggregate.props)
+
       val userId = UserId("user1")
       val userId2 = UserId("user2")
 
-      actorRef ! UserLoggedIn(userId)
-      expectMsgPF() {
+      probe.send(actorRef, UserLoggedIn(userId))
+      probe.expectMsgPF() {
         case LoggedInState(loggedInUsers) =>
           loggedInUsers must contain(userId)
           loggedInUsers must not contain (userId2)
       }
     }
 
-    "user logout" in {
+    "user logout" in new Actors {
+      val probe = TestProbe()
+      val actorRef = system.actorOf(LoginStateAggregate.props)
+
       val userId = UserId("user1")
       val userId2 = UserId("user2")
 
-      actorRef ! UserLoggedIn(userId)
-      expectMsg(LoggedInState)
-      actorRef ! UserLoggedIn(userId2)
-      expectMsg(LoggedInState)
-      actorRef ! UserLoggedOut(userId)
-      expectMsgPF() {
+      probe.send(actorRef, UserLoggedIn(userId))
+      probe.expectMsgType[LoggedInState]
+      probe.send(actorRef, UserLoggedIn(userId2))
+      probe.expectMsgType[LoggedInState]
+      probe.send(actorRef, UserLoggedOut(userId))
+      probe.expectMsgPF() {
         case LoggedInState(loggedInUsers) =>
           loggedInUsers must not contain (userId)
           loggedInUsers must contain(userId2)
       }
     }
   }
-
-  shutdown(system)
 }
